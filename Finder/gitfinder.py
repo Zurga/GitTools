ef425e046dfb44d7e57d29ca06316570a4fdbdaa
Jim <jim.lemmers@gmail.com>
2019-08-08 13:57:38 +0200

    threadpoolexecutor and low memory usage

--- Finder/gitfinder.py
+++ Finder/gitfinder.py
@@ -1,47 +1,88 @@
-#!/usr/bin/env python3
-
-'''
-Finder is part of https://github.com/internetwache/GitTools
-
-Developed and maintained by @gehaxelt from @internetwache
-
-Use at your own risk. Usage might be illegal in certain circumstances.
-Only for educational purposes!
-'''
-
-import argparse
-from functools import partial
-from multiprocessing import Pool
+#!/usr/bin/python
+import sys, os, argparse
 from urllib.request import urlopen
-from urllib.error import HTTPError
-import sys
+from functools import partial
+import requests
+from urllib.parse import urlparse
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import re
+from tqdm import tqdm
 
 
-def findgitrepo(output_file, domains):
-    domain = domains.strip()
+def findgitrepo(domain):
+    if domain:
+        domain = domain.strip()
 
+        try:
+            # Try to download http://target.tld/.git/HEAD
+            req = requests.get('http://{}/.git/HEAD'.format(domain), timeout=2)
+
+            # Check if refs/heads is in the file
+            if 'refs/heads' in req.text:
+                return req.url.replace('HEAD', '')
+            else:
+                return False
+
+        except Exception as e:
+            return
+
+def checkdotenv(domain):
+    if domain:
+        domain = domain.strip()
+
+        try:
+            # Try to download http://target.tld/.git/HEAD
+            req = requests.get('http://{}/.env'.format(domain), timeout=10)
+
+            # Check if refs/heads is in the file
+            if 'DB_PASS' in req.text:
+                return req.url
+            else:
+                return False
+
+        except Exception as e:
+            return
+
+def findsvn(domain):
+    if domain:
+        domain = domain.strip()
     try:
         # Try to download http://target.tld/.git/HEAD
-        with urlopen(''.join(['http://', domain, '/.git/HEAD']), timeout=5) as response:
-            answer = response.read(200).decode()
+        req = requests.get('http://{}/.svn/wc.db'.format(domain), timeout=10)
 
-    except HTTPError:
+        # Check if refs/heads is in the file
+        if req and 'wc.db' in req.url and 'text/html' \
+                not in req.headers['Content-Type']:
+            return req.url
+        else:
+            return False
+
+    except Exception as e:
         return
 
-    # Check if refs/heads is in the file
-    if 'refs/heads' not in answer:
+def testphp(domain):
+    if domain:
+        domain = domain.strip()
+    try:
+        # Try to download http://target.tld/.git/HEAD
+        req = requests.get('http://{}/test.php'.format(domain), timeout=10)
+
+        # Check if refs/heads is in the file
+        title = re.findall('<title>(.*)</title>', req.text)
+        if req and 'phpinfo()' in title:
+            return req.url
+        else:
+            return False
+
+    except Exception as e:
         return
 
-    # Write match to output_file
-    with open(output_file, 'a') as file_handle:
-        file_handle.write(''.join([domain, '\n']))
+def clean_url(url)
+    return url.replace('\n', '').replace('"', '')
 
-    print(''.join(['[*] Found: ', domain]))
-
-
-def read_file(filename):
-    with open(filename) as file:
-        return file.readlines()
+def prepare_requests(domain_file, executor, n):
+    return {executor.submit(globals()[func], clean_url(url)): clean_url(url)
+            for url in fle.readlines(n * 2)}
 
 def main():
     print("""
@@ -60,25 +101,39 @@ def main():
     parser.add_argument('-i', '--inputfile', default='input.txt', help='input file')
     parser.add_argument('-o', '--outputfile', default='output.txt', help='output file')
     parser.add_argument('-t', '--threads', default=200, help='threads')
+    parser.add_argument('-f', '--function', default='findgitrepo',
+                        help='functions: \n\tcheckdotenv \n\tfindgitrepo')
     args = parser.parse_args()
 
-    domain_file = args.inputfile
-    output_file = args.outputfile
     try:
         max_processes = int(args.threads)
     except ValueError as err:
         sys.exit(err)
 
-    try:
-        domains = read_file(domain_file)
-    except FileNotFoundError as err:
-        sys.exit(err)
 
-    fun = partial(findgitrepo, output_file)
-    print("Scanning...")
-    with Pool(processes=max_processes) as pool:
-        pool.map(fun, domains)
+    DOMAINFILE=args.inputfile
+    OUTPUTFILE=args.outputfile
+    MAXPROCESSES=int(args.threads)
+    func = args.function
+    found_repos = []
+
+    print('Reading urls from {file}.'.format(file=DOMAINFILE))
+    with open(DOMAINFILE, "r", buffering=1) as fle, ThreadPoolExecutor(max_workers=MAXPROCESSES) as executor:
+        future_to_url = prepare_requests(fle, executor, MAXPROCESSES)
+        while future_to_url:
+            print("Scanning...")
+            for future in tqdm(as_completed(future_to_url)):
+                url = future_to_url[future]
+                result = future.result()
+                if result:
+                    print("[*] Found: " + result)
+                    found_repos.append(result)
+            future_to_url = prepare_requests(fle, executor, MAXPROCESSES)
+    with open(OUTPUTFILE, 'w') as fle:
+        fle.writelines('\n'.join(found_repos))
+
     print("Finished")
+    # Write match to OUTPUTFILE
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
